0677aa24bd27ec698cc33df73a9d1e38f4b405bd
Isaac-moore <alabisheyi2001@yahoo.com>
2017-04-20 13:14:19 +0100

    Update predict_cp.py

--- predict_cp.py
+++ predict_cp.py
@@ -1,1 +1,17 @@
-predict_generator(self, generator, steps, max_q_size=10, workers=1, pickle_safe=False, verbose=0)
+from keras.models import model_from_json
+import numpy
+
+# load json and create model
+json_file = open('model.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+loaded_model = model_from_json(loaded_model_json)
+
+# load weights into new model
+loaded_model.load_weights("cookie.h5")
+ 
+# evaluate loaded model on test data
+loaded_model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+score = loaded_model.evaluate(X, Y, verbose=0)
+print "%s: %.2f%%" % (loaded_model.metrics_names[1], score[1]*100)
+
